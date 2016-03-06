88ce42205d5f9e35d482e3659332367d98833756
Petr Demin <peterdemin@gmail.com>
2016-03-06 12:06:20 -0500

    Updated release script

--- release.sh
+++ release.sh
@@ -1,10 +1,14 @@
-#!/bin/sh
+#!/bin/sh -e
 
+SOURCE_COMMIT=$(git log -1 --format='%H')
+pelican content
 git checkout master
+git fetch origin
+git reset --hard origin/master
 find . -maxdepth 1 -mindepth 1 -name '*' | grep -ve '\./\.' -e 'output' | xargs rm -rf
 mv output/* ./
 rmdir output
 git add *
-git commit -am "Generated pelican site"
+git commit -m "Generated pelican site from ${SOURCE_COMMIT}"
 git push
 git checkout -
