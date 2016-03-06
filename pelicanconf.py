fee401d29770b8b9bd33cc733c90886f17fdbae7
Petr Demin <peterdemin@gmail.com>
2016-03-06 14:19:57 -0500

    Fixing settings

--- pelicanconf.py
+++ pelicanconf.py
@@ -21,16 +21,19 @@ AUTHOR_FEED_ATOM = None
 AUTHOR_FEED_RSS = None
 
 # Blogroll
-LINKS = (('Pelican', 'http://getpelican.com/'),
-         ('Python.org', 'http://python.org/'),
-         ('Jinja2', 'http://jinja.pocoo.org/'),
-         ('You can modify those links in your config file', '#'),)
+LINKS = (
+    ('Pelican', 'http://getpelican.com/'),
+)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (
+    ('GitHub', 'https://github.com/peterdemin'),
+    ('LinkedIn', 'https://www.linkedin.com/in/peterdemin'),
+)
 
 DEFAULT_PAGINATION = False
 
 # Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
+
+TYPOGRIFY = True
