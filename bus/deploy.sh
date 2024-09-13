4d3363b5ff1fe7aa6ef60eceeacbd1e24bdfa8c1
Peter Demin <peterdemin@gmail.com>
2024-09-13 10:28:21 -0700

    Smarty pants

--- bus/deploy.sh
+++ bus/deploy.sh
@@ -2,10 +2,16 @@
 
 set -exo pipefail
 
-rm -rf ~bus/bus.bak
-mv ~bus/bus ~bus/bus.bak
+TARGET_DIR="~bus/bus"
+
+if test -d ${TARGET_DIR}
+then
+    rm -rf ${TARGET_DIR}.bak
+    mv ${TARGET_DIR} ${TARGET_DIR}.bak
+fi
 mv bus ~bus/
-chown -R bus:bus ~bus/bus
+chown -R bus:bus ${TARGET_DIR}
+
 systemctl daemon-reload
 systemctl restart bus.service
 systemctl status bus.service
