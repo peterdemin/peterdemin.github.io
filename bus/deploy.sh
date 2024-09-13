f5d2676b56f61af2508089bac01776f63f2d23ce
Peter Demin <peterdemin@gmail.com>
2024-09-13 10:22:39 -0700

    Draft deploy script

--- bus/deploy.sh
+++ bus/deploy.sh
@@ -2,4 +2,10 @@
 
 set -exo pipefail
 
-
+rm -rf ~bus/bus.bak
+mv ~bus/bus ~bus/bus.bak
+mv ~/bus ~bus/
+chown -R bus:bus ~bus/bus
+systemctl daemon-reload
+systemctl restart bus.service
+systemctl status bus.service
