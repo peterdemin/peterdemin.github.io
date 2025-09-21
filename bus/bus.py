1cc6c7034335733f7887ea82e96dbafc695ad7a8
Peter Demin <peterdemin@gmail.com>
2025-09-20 19:22:02 -0700

    Remove unused checkpoint API from bus API

--- bus/bus.py
+++ bus/bus.py
@@ -1,11 +1,8 @@
 import time
-import asyncio
-from typing import Optional
 
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from httpx import AsyncClient
-from pydantic import BaseModel
 
 app = FastAPI()
 app.add_middleware(
@@ -93,80 +90,4 @@ class ScheduleAPI:
         app.add_api_route(prefix + "/{stop_ids}", self.stop_times, methods=["GET"])
 
 
-class Metadata(BaseModel):
-    name: str
-    location: str
-    start_ts: float
-    finish_ts: float
-    context: str = ""
-    input_hint: str = ""
-    output_hint: str = ""
-
-
-class Checkpoint(BaseModel):
-    input: str
-    output: str
-    metadata: Metadata
-
-
-class CheckpointRepository:
-    def add(self, tenant: str, checkpoint: Checkpoint) -> None:
-        raise NotImplementedError()
-
-    def fetch_all(self, tenant: str) -> list[Checkpoint]:
-        raise NotImplementedError()
-
-
-class InMemoryCheckpointRepository(CheckpointRepository):
-    DEFAULT_CHECKPOINT_LIMIT = 100
-    DEFAULT_TTL = 60.0  # seconds
-
-    def __init__(
-        self, checkpoint_limit: int = DEFAULT_CHECKPOINT_LIMIT, ttl: float = DEFAULT_TTL
-    ) -> None:
-        self._storage: dict[str, list[tuple[float, Checkpoint]]] = {}
-        self._checkpoint_limit = checkpoint_limit
-        self._ttl = ttl
-
-    def add(self, tenant: str, checkpoint: Checkpoint) -> None:
-        self._trim_old(tenant)
-        if len(self._storage[tenant]) > self._checkpoint_limit:
-            self._storage[tenant] = self._storage[tenant][-self._checkpoint_limit:]
-        self._storage[tenant].append((time.time(), checkpoint))
-
-    def fetch_all(self, tenant: str) -> list[Checkpoint]:
-        self._trim_old(tenant)
-        return [v for _, v in self._storage[tenant]]
-
-    def _trim_old(self, tenant: str) -> None:
-        now = time.time()
-        self._storage[tenant] = [
-            (t, v) for t, v in (self._storage.get(tenant) or []) if now - t < self._ttl
-        ]
-
-
-class CheckpointAPI:
-    def __init__(self, checkpoint_repository: CheckpointRepository) -> None:
-        self._checkpoint_repository = checkpoint_repository
-
-    async def ingest_checkpoint(self, tenant: str, checkpoint: Checkpoint) -> dict:
-        self._checkpoint_repository.add(tenant, checkpoint)
-        asyncio.sleep(0.1)
-        return {"status": "ok"}
-
-    async def get_checkpoints(self, tenant: str) -> list[dict]:
-        return [
-            checkpoint.dict()
-            for checkpoint in self._checkpoint_repository.fetch_all(tenant)
-        ]
-
-    def register(self, app: FastAPI, prefix: str = "") -> None:
-        app.add_api_route(
-            prefix + "/{tenant}", self.ingest_checkpoint, methods=["POST"]
-        )
-        app.add_api_route(prefix + "/{tenant}", self.get_checkpoints, methods=["GET"])
-
-
 ScheduleAPI(Schedule()).register(app, "/bus")
-checkpoint_api = CheckpointAPI(InMemoryCheckpointRepository())
-checkpoint_api.register(app, "/i8t")
