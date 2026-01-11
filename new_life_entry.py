0ef6ae694c58ff45db6bc6b5ff8560d3ba2629a9
Peter Demin <peterdemin@gmail.com>
2026-01-10 20:25:47 -0800

    Jot down something

--- new_life_entry.py
+++ new_life_entry.py
@@ -1,27 +1,17 @@
 """Opens the right life file and prepolates it with the today's date"""
 # pylint: disable=consider-using-f-string
 import datetime
-import glob
 import os
 
 LIFE_DIR = os.path.join("source", "16_life")
 
 today = datetime.date.today()
-suffix = today.strftime("%Y-%m.md")
-matching_files = glob.glob(os.path.join(LIFE_DIR, f"??-{suffix}"))
-if matching_files:
-    result = matching_files[0]
-else:
-    existing_files = glob.glob(os.path.join(LIFE_DIR, "??-*.md"))
-    indexes = {os.path.basename(filename)[:2] for filename in existing_files}
-    indexes.discard("99")
-    next_index = "{:02}".format(int(max(indexes), 10) + 1)
-    result = os.path.join(LIFE_DIR, f"{next_index}-{suffix}")
-try:
+name = today.strftime("%Y-%m.md")
+result = os.path.join(LIFE_DIR, name)
+content: str = ''
+if os.path.exists(result):
     with open(result, "rt", encoding="utf-8") as fp:
         content = fp.read()
-except FileNotFoundError:
-    content = ''
 with open(result, "wt", encoding="utf-8") as fp:
     header = today.strftime("%b %d, %Y")
     fp.write(f"`{header}` - \n\n")
