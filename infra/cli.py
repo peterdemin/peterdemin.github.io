b845ff18543a736250d0e0680f13a7e45aa45723
Peter Demin <peterdemin@gmail.com>
2026-02-07 01:21:17 -0800

    Remove debugger

--- infra/cli.py
+++ infra/cli.py
@@ -161,7 +161,6 @@ class BuilderPublishCommand:
 
     def handle(self, args):
         mirrors = self._load_mirrors()
-        import pdb; pdb.set_trace()
         self._push_content(args.content, mirrors)
         return self._push_infra(mirrors)
 
