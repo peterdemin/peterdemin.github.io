ab441af3e8f2a0969262155fc6d637d7efbf43ff
Peter Demin <peterdemin@gmail.com>
2026-02-11 21:29:12 -0800

    Remove GitHub repo block

--- source/conf.py
+++ source/conf.py
@@ -1,42 +1,17 @@
-"""
-Configuration file for the Sphinx documentation builder.
-
-This file only contains a selection of the most common options. For a full
-list see the documentation:
-https://www.sphinx-doc.org/en/master/usage/configuration.html
-"""
+"""Configuration file for the Sphinx documentation builder."""
 # pylint: disable=invalid-name, redefined-builtin
 
 from sphinx.builders.dirhtml import DirectoryHTMLBuilder
-
-# Ensure WebP is considered an image for HTML output
-if "image/webp" not in DirectoryHTMLBuilder.supported_image_types:
-    DirectoryHTMLBuilder.supported_image_types.append("image/webp")
-
-# -- Path setup --------------------------------------------------------------
-
-# If extensions (or modules to document with autodoc) are in another directory,
-# add these directories to sys.path here. If the directory is relative to the
-# documentation root, use os.path.abspath to make it absolute, like shown here.
-#
-# import os
-# import sys
-# sys.path.insert(0, os.path.abspath('.'))
-
-
-# -- Project information -----------------------------------------------------
 
 project = "Peter Demin"
 html_title = "Peter Demin"
 copyright = "2022, Peter Demin"
 author = "Peter Demin"
 
+# Ensure WebP is considered an image for HTML output
+if "image/webp" not in DirectoryHTMLBuilder.supported_image_types:
+    DirectoryHTMLBuilder.supported_image_types.append("image/webp")
 
-# -- General configuration ---------------------------------------------------
-
-# Add any Sphinx extension module names here, as strings. They can be
-# extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
-# ones.
 extensions = [
     "myst_parser",
     "sphinx_design",
@@ -48,39 +23,15 @@ myst_enable_extensions = [
     "attrs_block",
 ]
 
-# Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
-
-# List of patterns, relative to source directory, that match files and
-# directories to ignore when looking for source files.
-# This pattern also affects html_static_path and html_extra_path.
-exclude_patterns = []
-
 extlinks = {
     "GH": ("https://github.com/%s", "%s"),
 }
 
-
-# -- Options for HTML output -------------------------------------------------
-
-# The theme to use for HTML and HTML Help pages.  See the documentation for
-# a list of builtin themes.
-#
 html_theme = "shibuya"
 html_theme_options = {
     "page_layout": "compact",
 }
-html_context = {
-    "source_type": "github",
-    "source_user": "peterdemin",
-    "source_repo": "peterdemin.github.io",
-    "source_version": "master",
-    "source_docs_path": "/source/",
-}
-
-# Add any paths that contain custom static files (such as style sheets) here,
-# relative to this directory. They are copied after the builtin static files,
-# so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ["_static"]
 html_css_files = [
     "custom.css",
