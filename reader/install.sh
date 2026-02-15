a507030b40e7fa58bde82c0608d88f2034027552
Peter Demin <peterdemin@gmail.com>
2026-02-15 13:25:41 -0800

    Revert "Move reader inside of VPN"
    This reverts commit 8f713d9302dbd52adfbd555e948eadc1d904d15e.

--- reader/install.sh
+++ reader/install.sh
@@ -4,6 +4,7 @@ id reader || useradd -rs /sbin/nologin reader
 cp -rf reader/etc/systemd/system/* /etc/systemd/system/
 cp -f reader/etc/nginx/sites-available/reader /etc/nginx/sites-available/reader
 ln -sf /etc/nginx/sites-available/reader /etc/nginx/sites-enabled/reader
+sudo certbot --nginx -nd reader.demin.dev
 systemctl daemon-reload
 systemctl disable reader-api.service
 systemctl stop reader-api.service
